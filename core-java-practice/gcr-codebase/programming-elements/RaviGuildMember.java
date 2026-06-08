d6feba58e749954146371ecc4e4a8656462345ff
rishigupta-cyber <rishiguptaorai04@gmail.com>
2026-06-08 13:39:16 +0530

    Add programming elements files

---
+++ core-java-practice/gcr-codebase/programming-elements/RaviGuildMember.java
@@ -0,0 +1,11 @@
+public class RaviGuildMember {
+    public static void main(String[] args) {
+        String name="Ravi";
+        int age= 24;
+        char ch= 'A';
+        float memberFee= 700.0f;
+        double salary = 35000.00;
+        int salaryBonus= (int)(salary * 0.12);
+        System.out.println(salaryBonus);
+    }    
+}
